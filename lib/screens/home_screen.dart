b2a07c74d3d08d7ebe01ecdad58ec118a218a153
Scott Matthews <scottmatthews343@gmail.com>
2022-04-27 23:36:33 +0100

    reactor icons into badge_icon widget

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -8,6 +8,7 @@ import 'package:todo/providers/task_provider.dart';
 import 'package:todo/screens/pinned_screen.dart';
 import 'package:todo/screens/routines_screen.dart';
 import 'package:todo/screens/tasks_screen.dart';
+import '../widgets/badge_icon.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -46,7 +47,6 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // TODO is this a scaffold?
       body: _pages[_selectedIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
@@ -54,43 +54,22 @@ class _HomeScreenState extends State<HomeScreen> {
         items: [
           BottomNavigationBarItem(
             label: '',
-            icon: Stack(
-              children: [
-                Icon(Entypo.list),
-                _buildNotificationBlip(
-                    Provider.of<TaskProvider>(context).isDueCount),
-              ],
-            ),
+            icon: Icon(Entypo.list),
           ),
           BottomNavigationBarItem(
             label: '',
             icon: Icon(Entypo.pin),
           ),
           BottomNavigationBarItem(
-            label: '',
-            // icon: Icon(Entypo.cw),
-            //icon: Icon(Entypo.circular_graph),
-            icon: Stack(
-              children: <Widget>[
-                Transform(
+              label: '',
+              icon: BadgeIcon(
+                icon: Transform(
                   transform: Matrix4.rotationY(pi),
                   alignment: Alignment.center,
                   child: Icon(Entypo.circular_graph),
                 ),
-                _buildNotificationBlip(
-                    Provider.of<RoutineProvider>(context).isDueCount),
-              ],
-            ),
-            // icon: Stack(
-            //   children: [
-            //     Icon(Entypo.circle_with_plus),
-            //     Icon(Entypo.pin)
-            //     // Align(
-            //     //   alignment: Alignment.topRight,
-            //     //   child: Icon(Entypo.pin),
-            //     // )
-            //   ],
-          ),
+                badgeCount: Provider.of<RoutineProvider>(context).isDueCount,
+              )),
         ],
       ),
     );
